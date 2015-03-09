e0adb79dda1b1545db2476980bb11cd30e9f7bcf
martabajo <martabg2492@gmail.com>
2015-03-09 10:06:52 +0100

    Modificada clase NameGenerator

--- NameGenerator.java
+++ NameGenerator.java
@@ -10,28 +10,27 @@ public class NameGenerator
     // instance variables - replace the example below with your own
     private String apellido;
     private String nombre;
-    private String apellidoMadre;
-    private String ciudadNacimiento;
-    private static final int TRES_PRI_LETRAS_APELLIDO = 0;
-    private static final int TRES_PRI_LETRAS_APELLIDO_FINAL = 2;
-    private static final int TRES_PRI_LETRAS_NOMBR = 0;
-    private static final int TRES_PRI_LETRAS_NOMBR_FINAL = 2;
-    private static final int DOS_PRI_LETRAS_APELL_MADRE = 0;
-    private static final int DOS_PRI_LETRAS_APELL_MADRE_FINAL = 1;
-    private static final int TRES_PRI_LETRAS_CIUDAD_NACI = 0;
-    private static final int TRES_PRI_LETRAS_CIUDAD_NACI_FINAL = 2;
+    
+    private static final int VALORES_INICIALES = 0;
+    private static final int TRES_PRI_LETRAS_APELLIDO = 3;
+    private static final int TRES_PRI_LETRAS_NOMBR = 3;
+    private static final int DOS_PRI_LETRAS_APELL_MADRE = 2;
+    private static final int TRES_PRI_LETRAS_CIUDAD_NACI = 3;
 
     /**
      * Constructor for objects of class NameGenerator
      */
-    public NameGenerator(String apellido, String nombre, String apellidoMAdre, String ciudadNacimiento)
+    public NameGenerator()
     {
         // initialise instance variables
-       this.apellido = apellido;
-       this.nombre = nombre;
-       this.apellidoMadre = apellidoMadre;
-       this.ciudadNacimiento = ciudadNacimiento;
+       nombre = null;
+       apellido = null;
+    }
+    
+    public void generateStarWarsName(String apellido, String nombre, String apellidoMadre, String ciudadNacimiento)
+    {
+    System.out.println("Tu nombre en Star Wars es: " + apellido.substring(VALORES_INICIALES , TRES_PRI_LETRAS_APELLIDO) + nombre.substring(VALORES_INICIALES , TRES_PRI_LETRAS_NOMBR));
+    System.out.println("Tu apellido en Star Wars es: " + apellidoMadre.substring(VALORES_INICIALES, DOS_PRI_LETRAS_APELL_MADRE) + ciudadNacimiento.substring(VALORES_INICIALES,TRES_PRI_LETRAS_CIUDAD_NACI));
     }
 
- 
 }
